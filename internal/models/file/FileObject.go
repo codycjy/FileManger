10e2fa25e32f6fe7264e31992b9156a8215cac0e
fish-portable <cody741963@hotmail.com>
2023-03-25 22:23:51 +0800

    feat(model): add user and file model

--- internal/models/file/FileObject.go
+++ internal/models/file/FileObject.go
@@ -1,1 +1,21 @@
 package file
+
+import "gorm.io/gorm"
+
+type FileObject struct {
+	gorm.Model
+	FileName string
+	FileType string
+	Path     string
+	OwnerID  uint
+}
+
+type Folder struct {
+	*FileObject
+	Files []FileObject
+}	
+
+type File struct {
+	*FileObject
+	Size int64
+}
