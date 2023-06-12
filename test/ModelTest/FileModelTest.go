92473bb17f20112e2af524b381fe820df0b705c4
codycjy <cody741963@hotmail.com>
2023-06-12 20:31:00 +0800

    test: add complicated relationship for tests

--- test/ModelTest/FileModelTest.go
+++ test/ModelTest/FileModelTest.go
@@ -11,12 +11,23 @@ type Content interface {
 	GetID() uint
 }
 
+func DropTables() {
+	var tables = []string{"user_files", "user_folders",}
+	db:=mysql.GetDB()
+	for _, table := range tables {
+		db.Exec("DROP TABLE IF EXISTS " + table)
+	}
+	db.Migrator().DropTable(&models.User{}, &models.File{}, &models.Folder{})
+}
+
 type User models.User
 type File models.File
 type Folder models.Folder
 func main() {
 	// Implement GORM connection and initialization
 	db:=mysql.GetDB()
+
+	DropTables()
 	db.AutoMigrate(&models.User{}, &models.File{}, &models.Folder{},)
 
 	// Create a user
@@ -38,7 +49,7 @@ func main() {
 	db.Create(&rootFolder)
 
 	// Create a nested folder inside the root folder
-	nestedFolder := Folder{
+	nestedFolder := models.Folder{
 		Name:     "Nested",
 		Path:     "/Root/Nested",
 		UserID:   user.ID,
@@ -46,18 +57,38 @@ func main() {
 	}
 
 	db.Create(&nestedFolder)
+	nestedFolder2 := models.Folder{
+		Name:     "Nested2",
+		Path:     "/Root/Nested2",
+		UserID:   user.ID,
+		ParentID: &rootFolder.ID,
+	}
+	db.Create(&nestedFolder2)
+	nestedFolder3 := models.Folder{
+		Name:     "Nested3",
+		Path:     "/Root/Nested2/Nested3",
+		UserID:   user.ID,
+		ParentID: &nestedFolder2.ID,
+	}
 
+
+	db.Create(&nestedFolder3)
+
+	rootFolder.Children = append(rootFolder.Children, &nestedFolder,&nestedFolder2)
+	nestedFolder2.Children=append(nestedFolder2.Children, &nestedFolder3)
+	db.Save(&rootFolder)
+	db.Save(&nestedFolder2)
 	// Create a file inside the nested folder
 	file := File{
 		FileName: "testfile.txt",
 		Path:     "/Root/Nested/testfile.txt",
 		Size:     1024,
 		UserID:   user.ID,
-		FolderID: nestedFolder.ID,
+		FolderID: nestedFolder2.ID,
 	}
 
 	db.Create(&file)
-// Add the file to the user's Files field
+	// Add the file to the user's Files field
 	user.Files = append(user.Files, models.File(file))
 	db.Save(&user)
 	// Query the database and print the results
